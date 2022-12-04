0b461e5a8654462cfb1ffb3b4344944a64e67d4f
Francisco Peixoto <thtr.irubra@gmail.com>
2022-12-04 00:08:41 +0000

    coinflip

--- coinflip.tsx
+++ coinflip.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { XNFTProvider, XNFTCard } from 'react-xnft';
 
 const CoinFlip = () => {
@@ -9,10 +9,50 @@ const CoinFlip = () => {
   // The XNFT token ID of the coin being flipped
   const coinTokenId = '0x...';
 
+  // Initialize state variables to track the result of the coin flip
+  // and whether the game is in progress or not
+  const [result, setResult] = useState(null);
+  const [inProgress, setInProgress] = useState(false);
+
+  // Function to flip the coin and determine the result
+  const flipCoin = () => {
+    // Set the game to be in progress
+    setInProgress(true);
+
+    // Generate a random number between 0 and 1
+    const randNum = Math.random();
+
+    // If the random number is less than 0.5, set the result to "heads"
+    // Otherwise, set the result to "tails"
+    if (randNum < 0.5) {
+      setResult("heads");
+    } else {
+      setResult("tails");
+    }
+  };
+
+  // Function to reset the game and start over
+  const reset = () => {
+    setResult(null);
+    setInProgress(false);
+  };
+
+  // Render the coin flip game UI
   return (
     <XNFTProvider contractAddress={contractAddress} walletAddress={walletAddress}>
       <XNFTCard tokenId={coinTokenId} />
-      <button>Flip Coin</button>
+      {inProgress ? (
+        <>
+          {/* If the game is in progress, show the result of the coin flip */}
+          <p>Result: {result}</p>
+          <button onClick={reset}>Reset</button>
+        </>
+      ) : (
+        <>
+          {/* If the game is not in progress, show the button to start the coin flip */}
+          <button onClick={flipCoin}>Flip Coin</button>
+        </>
+      )}
     </XNFTProvider>
   );
 };
